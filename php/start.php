af7a67c38f71b08f3c8ee31a52139418a4e4279e
alexandraalmo <alexandra.almo@jayway.com>
2017-01-29 21:33:51 +0100

    Changed buttons on start page and removed a tag.

--- php/start.php
+++ php/start.php
@@ -25,19 +25,11 @@
     #Deltävling 2: Malmö 11/2 2017
     #Deltävling 3: Växjö 18/2 2017
     #Deltävling 4: Skellefteå 25/2 2017
-
-    
-    
-    
 ?>
-
 
 <div class="row"><div class="col-12 linksheader">DELTÄVLINGAR</div></div>
 
-<div class="row link-button"><button><a href="?page=contest&contest=1" >1. GÖTEBORG</a></button></div>
-<div class="row link-button"><button><a href="?page=contest&contest=2">2. MALMÖ</a></button></div>
-<div class="row link-button"><button><a href="?page=contest&contest=3">3. VÄXJÖ</a></button></div>
-<div class="row link-button"><button><a href="?page=contest&contest=4">4. SKELLEFTEÅ</a></button></div>
-
-
-
+<div class="row link-button"><button onClick="window.location='?page=contest&contest=1';" >1. GÖTEBORG</button></div>
+<div class="row link-button"><button onClick="window.location='?page=contest&contest=2';" >2. MALMÖ</button></div>
+<div class="row link-button"><button onClick="window.location='?page=contest&contest=3';" >3. VÄXJÖ</button></div>
+<div class="row link-button"><button onClick="window.location='?page=contest&contest=4';" >4. SKELLEFTEÅ</button></div>
