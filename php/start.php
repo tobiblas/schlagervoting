211ee082b256a911081aaf272decb6c62b1bbeda
alexandraalmo <alexandra.almo@jayway.com>
2017-02-01 23:15:24 +0100

    Added a overlay with contest points info.

--- php/start.php
+++ php/start.php
@@ -28,6 +28,14 @@ function loadContest(number) {
     }
 }
 
+function openNav() {
+    document.getElementById("myNav").style.width = "100%";
+}
+
+function closeNav() {
+    document.getElementById("myNav").style.width = "0%";
+}
+
 </script>
 
 <?php
@@ -44,3 +52,40 @@ function loadContest(number) {
 <div class="row link-button"><button onClick="loadContest(2);" >2. MALMÖ</button></div>
 <div class="row link-button"><button onClick="loadContest(3);" >3. VÄXJÖ</button></div>
 <div class="row link-button"><button onClick="loadContest(4);" >4. SKELLEFTEÅ</button></div>
+
+
+<div id="myNav" class="overlay">
+
+  <!-- Button to close the overlay navigation -->
+  <div class="closebtn" onClick="closeNav()">&times;</div>
+
+  <!-- Overlay content -->
+  <div class="overlay-content">
+    <div class="row"><div class="col-12 linksheader">Poängberäkning Deltävling</div></div>
+    <br>
+    <div class="row"><div class="col-12 center">FÖR VARJE BIDRAG FÅR MAN:</div></div>
+    <br>
+    <div class="row"><div class="col-12" style="padding: 3 3 3 10"><b>6 poäng</b> om du satte den i rätt grupp (direkt vidare/andra chansen/utslagen)</div></div>
+    <div class="row"><div class="col-12" style="padding: 3 3 3 10"><b>3 poäng</b> om du satte den 1 steg ifrån rätt grupp (exempel: du satte direkt vidare men den hamnade i andra chansen)</div></div>
+    <div class="row"><div class="col-12" style="padding: 3 3 30 10"><b>1 poäng</b> om du satte den 2 steg ifrån rätt grupp (exempel: du satte direkt vidare men den hamnade i utslagen)</div></div>
+
+    <br>
+    <div class="row"><div class="col-12 center">EXTRA:</div></div>
+    <br>
+    <div class="row"><div class="col-12" style="padding: 3 3 3 10"><b>2 poäng</b> om låt på plats 5 var rätt</div></div>
+    <div class="row"><div class="col-12" style="padding: 3 3 3 10"><b>2 poäng</b> om låt på plats 6 var rätt</div></div>
+    <div class="row"><div class="col-12" style="padding: 3 3 3 10"><b>4 poäng</b> om låt på plats 7 var rätt</div></div>
+
+    <div class="row"><div class="col-12" style="padding: 3 3 3 10"><b>-5 poäng</b> om du satte låt på plats 7 som gick direkt vidare</div></div>
+
+    <br><br>
+    <div class="row"><div class="col-12 center">MAX poäng: 50</div></div>
+
+    <div class="row"><div class="col-12 center">MIN poäng: 10</div></div>
+  </div>
+
+</div>
+
+<div id="start-footer" onClick="openNav()">Så här funkar poängberäkningen</div>
+
+
