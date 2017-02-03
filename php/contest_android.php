a39ccd49873983adb0035986b6ae90644710d30c
tobiblas <tobias2.jonsson@gmail.com>
2017-02-03 22:25:51 +0100

    Correct order

--- php/contest_android.php
+++ php/contest_android.php
@@ -29,10 +29,34 @@
     
     $artists = array
     (
-     array(new Artist("Adrijana","Amare"), new Artist("Boris René", "Her kiss"), new Artist("Nano", "Hold on"),new Artist("Charlotte Perrelli","Mitt liv"),new Artist("Dinah Nah","One more night"),new Artist("De vet du","Road trip"),new Artist("Ace Wilder","Wild child")),
-     array(new Artist("Mariette","A million years"), new Artist("Benjamin Ingrosso","Good loving"), new Artist("Roger Pontare", "Himmel och hav"),new Artist("Lisa Ajax","I don't giva a"),new Artist("Etzia","Up"),new Artist("Dismissed","Hearts alined"),new Artist("Allyawan","Vart haru varit")),
-     array(new Artist("Owe Thörnqvist","Boogieman blues"), new Artist("Bella & Filippa","Crucified"), new Artist("The Fooo Conspiracy", "Gotta thing"),new Artist("Jasmine Kara","Gravity"),new Artist("Robin Bengtsson","I can’t go on"),new Artist("Angton hagman","Kiss you goodbye"),new Artist("Krista Sigfrieds","Snurra min jord")),
-     array(new Artist("Wiktoria Johansson","As I lay me down"), new Artist("Les Gordons","Bound to fall"), new Artist("Sara Varga & Juha Mulari", "Du får inte ändra på mig"),new Artist("Jon Henrik Fjellgren feat Aninia","En värld full av strider"),new Artist("Axel Schylström","När ingen ser"),new Artist("Alice Svensson","Running with lions"),new Artist("Loreen","Statements"))
+     array(new Artist("Boris René", "Her kiss"),
+           new Artist("Adrijana","Amare"),
+           new Artist("Dinah Nah","One more night"),
+           new Artist("De vet du","Road trip"),
+           new Artist("Charlotte Perrelli","Mitt liv"),
+           new Artist("Ace Wilder","Wild child"),
+           new Artist("Nano", "Hold on")),
+     array(new Artist("Mariette","A million years"),
+           new Artist("Roger Pontare", "Himmel och hav"),
+           new Artist("Etzia","Up"),
+           new Artist("Allyawan","Vart haru varit"),
+           new Artist("Dismissed","Hearts alined"),
+           new Artist("Lisa Ajax","I don't giva a"),
+           new Artist("Benjamin Ingrosso","Good loving")),
+     array(new Artist("Robin Bengtsson","I can’t go on"),
+           new Artist("Krista Sigfrieds","Snurra min jord"),
+           new Artist("Angton hagman","Kiss you goodbye"),
+           new Artist("Jasmine Kara","Gravity"),
+           new Artist("Owe Thörnqvist","Boogieman blues"),
+           new Artist("Bella & Filippa","Crucified"),
+           new Artist("The Fooo Conspiracy", "Gotta thing about you")),
+     array(new Artist("Jon Henrik Fjellgren feat Aninia","En värld full av strider"),
+           new Artist("Alice Svensson","Running with lions"),
+           new Artist("Les Gordons","Bound to fall"),
+           new Artist("Wiktoria Johansson","As I lay me down"),
+           new Artist("Axel Schylström","När ingen ser"),
+           new Artist("Sara Varga & Juha Mulari", "Du får inte ändra på mig"),
+           new Artist("Loreen","Statements"))
      );
     
     $contestants = $artists[$contest];
